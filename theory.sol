1a6a2f368128aea141a94f44fc186d294b716a6d
naraSokami <derwaeldaltonbaptiste@gmail.com>
2022-10-06 16:07:25 +0200

    initial commit

---
+++ theory.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+
+// SOLIDITY
+
+/*
+
+    contracts
+    .sol -> compile -> deploy
+    receive()
+    types => address, uint256, bool, string memory/storage
+    mapping => mapping(type1 => type2) Map; / Map[type1Var]
+    struct => StructName { type property; type property; }
+    payableAddress.tranfer(_to, _amount)
+    transfer
+
+*/
